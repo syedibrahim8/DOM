e59b4d25617c56d4bedaa61adcf03867f3701b13
syedibrahim8 <syedibrahimofficial1@gmail.com>
2026-01-02 15:29:09 +0000

    dom methods

--- script.js
+++ script.js
@@ -24,6 +24,79 @@
 // text.innerHTML = "<h1>Hello</h1>"
 // console.log(text);
 
+// 7. createElement("")
+// 8.append()
+// let div = document.getElementById("intro");
+// console.log(div);
+// let heading = document.createElement("h1");
+// let text = "hello";
+// heading.append(text);
+// div.append(heading);
+
+// 9.removeElement()
+//  let ul = document.getElementById("fruits")
+//  console.log(ul);
+// // firstElementChild -> first element of the list
+// // firstElementChild.nextElementSibling -> next sibling of first element
+//  let li = ul.firstElementChild.nextElementSibling
+//  console.log(li);
+//  ul.removeChild(li) // -> to remove selected list item
+
+// 10.setAttribute("attributeName","value")
+    // let btn = document.getElementById("btn")
+    // btn.setAttribute("style","background-color:red")
+
+// 11.getAttribute("attributeName")
+    // let btn = document.getElementById("btn")
+    // let atr = btn.getAttribute("id")
+    // console.log(atr);
+
+// 12.style
+    // let btn = document.getElementById("btn")
+    // btn.style.color = "white"
+    // btn.style.backgroundColor = "red"
+    // // btn.style.cssText = "background-color:red" -> another way to add style through DOM
+
+// 13.getComputedStyle("#")
+// let btn = document.getElementById("btn")
+// let styles = getComputedStyle(btn);
+// console.log(styles)
+// console.log(styles.background);
+// console.log(styles.color);
+
+// 14. cssClasses()
+    // let btn = document.getElementById("btn")
+    // btn.className = "mybtn cfi" // adds classname with value given & can be used for predefined css with the same class value
+    // console.log(btn.classList);
+    // btn.classList.remove("cfi") // removes class
+    // console.log(btn.classList);
+    // btn.classList.replace("mybtn","hibtn"); // replace class name
+    // console.log(btn.classList);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // 7. navigator.geolocation.getCurrentPosition(map=>{},error=>{})
 // navigator.geolocation.getCurrentPosition(
 //     map=>{
@@ -35,3 +108,5 @@
 //         console.log(error);
 //     }
 // )
+
+
